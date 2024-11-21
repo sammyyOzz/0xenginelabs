670ce161dff6e8f4e14ce031bf27c6ea5bfe863f
sammy <sammyoziegbe@gmail.com>
2024-11-21 12:12:33 +0100

    client card animations

---
+++ src/components/clients-desktop.tsx
@@ -0,0 +1,198 @@
+import { Box, Flex } from "@chakra-ui/react";
+import Image from "next/image";
+import xammColor from "@/assets/images/xamm-color.png";
+import xammBlack from "@/assets/images/xamm-black.png";
+import xamm2 from "@/assets/images/xamm-2.png";
+import myrkleBlack from "@/assets/images/myrkle-black.png"
+import myrkleColor from "@/assets/images/myrkle-color.png"
+import myrkleSparkLeft from "@/assets/images/myrkle-spark-left.png"
+import myrkleSparkRight from "@/assets/images/myrkle-spark-right.png"
+import walkitBlack from "@/assets/images/walkit-black.png"
+import walkitColor from "@/assets/images/walkit-color.png"
+import walkit2 from "@/assets/images/walkit-2.png"
+
+function ClientsDesktop() {
+  return (
+    <>
+      <Box
+        zIndex={3}
+        pos="absolute"
+        w="45%"
+        boxShadow="0 0 20px #9c9b9b"
+        bg="#ffffff"
+        aspectRatio={1 / 1.7}
+        transition="200ms linear all"
+        overflow="hidden"
+        _hover={{
+          transition: "200ms linear all",
+          bg: "#000000",
+          borderRadius: "25px",
+          "& .icon-container": {
+            top: "20%",
+            transition: "200ms linear all",
+          },
+          "& .xamm-black": {
+            opacity: 0
+          },
+          "& .xamm-bottom": {
+            bottom: 0,
+            left: 0,
+            transition: "200ms linear all",
+          }
+        }}
+      >
+        <Box
+          className="icon-container"
+          pos="absolute"
+          w="100px"
+          h="100px"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          transition="200ms linear all"
+        >
+          <Image src={xammColor} alt="" style={{ position: "absolute" }} />
+          <Image
+            className="xamm-black"
+            src={xammBlack}
+            alt=""
+            style={{ position: "absolute" }}
+          />
+        </Box>
+        <Box className="xamm-bottom" pos="absolute" bottom="-50%" left="-50%" transition="200ms linear all">
+          <Image src={xamm2} alt="" />
+        </Box>
+      </Box>
+
+      <Box
+        zIndex={2}
+        pos="absolute"
+        top="200px"
+        left="250px"
+        boxShadow="0 0 20px #9c9b9b"
+        bg="#ffffff"
+        w="45%"
+        aspectRatio={1 / 1.7}
+        transition="200ms linear all"
+        overflow="hidden"
+        css={{
+          "& .myrkle-color": {
+            pos: "absolute",
+            mt: "30px",
+            opacity: 0,
+            transform: "scale(3)",
+            transition: "200ms linear all",
+          },
+          "& .myrkle-spark-left": {
+            pos: "absolute",
+            bottom: "70px",
+            left: "-50%",
+            transition: "200ms linear all",
+          },
+          "& .myrkle-spark-right": {
+            pos: "absolute",
+            bottom: "70px",
+            right: "-50%",
+            transition: "200ms linear all",
+          },
+        }}
+        _hover={{
+          transition: "200ms linear all",
+          bg: "#000000",
+          borderRadius: "25px",
+          "& .xamm-bottom": {
+            bottom: 0,
+            left: 0,
+            transition: "200ms linear all",
+          },
+          "& .myrkle-color": {
+            transform: "scale(1.5)",
+            opacity: 1
+          },
+          "& .myrkle-spark-left": {
+            left: 0,
+            transition: "200ms linear all",
+          },
+          "& .myrkle-spark-right": {
+            right: 0,
+            transition: "200ms linear all",
+          },
+        }}
+      >
+        <Box
+          pos="absolute"
+          w="100px"
+          h="100px"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          transition="200ms linear all"
+        >
+          <Image
+            className="myrkle-black"
+            src={myrkleBlack}
+            alt=""
+            style={{ position: "absolute" }}
+          />
+          <Image className="myrkle-color" src={myrkleColor} alt="" />
+        </Box>
+          <Image className="myrkle-spark-left" src={myrkleSparkLeft} alt="" />
+          <Image className="myrkle-spark-right" src={myrkleSparkRight} alt="" />
+      </Box>
+
+      <Box
+        zIndex={1}
+        pos="absolute"
+        top="100px"
+        left="500px"
+        boxShadow="0 0 20px #9c9b9b"
+        bg="#ffffff"
+        w="45%"
+        aspectRatio={1 / 1.7}
+        transition="200ms linear all"
+        overflow="hidden"
+        _hover={{
+          transition: "200ms linear all",
+          bg: "#000000",
+          borderRadius: "25px",
+          "& .icon-container": {
+            top: "20%",
+            transition: "200ms linear all",
+          },
+          "& .xamm-black": {
+            opacity: 0
+          },
+          "& .xamm-bottom": {
+            bottom: 0,
+            left: 0,
+            transition: "200ms linear all",
+          }
+        }}
+      >
+        <Box
+          className="icon-container"
+          pos="absolute"
+          w="100px"
+          h="100px"
+          top="50%"
+          left="50%"
+          transform="translate(-50%, -50%)"
+          transition="200ms linear all"
+        >
+          <Image src={walkitColor} alt="" style={{ position: "absolute" }} />
+          <Image
+            className="xamm-black"
+            src={walkitBlack}
+            alt=""
+            style={{ position: "absolute" }}
+          />
+        </Box>
+        <Box className="xamm-bottom" pos="absolute" bottom="-50%" left="-50%" transition="200ms linear all">
+          <Image src={walkit2} alt="" />
+        </Box>
+      </Box>
+    </>
+  );
+}
+
+export default ClientsDesktop;
--- src/components/sections/client-section.tsx
+++ src/components/sections/client-section.tsx
@@ -3,6 +3,7 @@ import Container from "../ui/container";
 import { Box, Flex, Text } from "@chakra-ui/react";
 import SettingsIcon from "../icons/settins";
 import { Button } from "../ui/button";
+import ClientsDesktop from "../clients-desktop";
 
 function ClientSection() {
   return (
@@ -10,15 +11,15 @@ function ClientSection() {
       <Container>
         <SettingsIcon fontSize="90px" />
 
-        <Flex gap="100px">
-          <Box w="50%" pt={20}>
+        <Flex gap="100px" direction={["column", null, null, "row"]}>
+          <Box w={["100%", null, null, "50%"]} pt={20} pb={[0, null, null, 40]}>
             <Text color="gray" textTransform="uppercase" fontSize="lg">
               here are
             </Text>
             <Text color="#000000" fontSize="6xl" fontWeight="black" mb={8}>
               A few of our clients
             </Text>
-            <Text color="gray" w="80%" fontSize="xl" mb={20}>
+            <Text color="gray" w={["100%", null, null, "80%"]} fontSize="xl" mb={20}>
               Meet our delighted clients who trust us for exceptional service
               and quality. Their satisfaction and positive feedback inspire us
               to continually exceed expectations and deliver outstanding
@@ -30,7 +31,9 @@ function ClientSection() {
             </Button>
           </Box>
 
-          <Box w="50%"></Box>
+          <Box w={["100%", null, null, "50%"]} pos="relative" border="1px solid red">
+            <ClientsDesktop />
+          </Box>
         </Flex>
       </Container>
     </Box>
